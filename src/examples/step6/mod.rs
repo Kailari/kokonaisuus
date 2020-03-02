d948b9a382800d2834eaa4b407acc5109d12dae4
toilari <ilari97@hotmail.com>
2020-03-02 06:04:17 +0200

    Tweaks to the part 6, added component storage and dispatcher as dumb-implementations

--- src/examples/step6/mod.rs
+++ src/examples/step6/mod.rs
@@ -6,6 +6,8 @@ New here:
     -   Systems now use a shared `System`-trait, which provides a `tick()`-method
     -   System arguments are defined using associated type on system trait
     -   There are lifetimes required! :o (sadly, they are required for odd reasons, but anyways)
+    -   Cut-pasted the system initialization and `tick` calls to a new struct `Dispatcher`
+    -   Cut-pasted the component initialization to a new struct `ComponentStorage`
 
 Notes:
     In order to be able to start planning the dispatcher, first thing is to unify the way we handle
@@ -17,61 +19,34 @@ Notes:
     a tad bit of complexity to how we can handle systems requiring varying number of component
     vectors of all sorts of types, but there is nothing too fancy going on.
 
+    In addition to that, I wanted to start switching the mindset away from initializing and doing
+    everything in `main()`, so systems and components were wrapped into their own structs,
+    `Dispatcher` and `ComponentStorage`, respectively. This should allow us to start setting up
+    required framework for unifying the system execution.
 
-    Next up, we need to start taking steps towards centralizing component storage, so that we could
-    automatically determine which component vectors to pass to `.tick()`. I have very vague idea on
-    how this can be done, but it might prove challenging.
+
+    Next up, we need to start taking steps towards further refining component storage, so that we
+    could automatically determine which component vectors to pass to `.tick()`. I have very vague
+    idea on how this can be done, but it might prove challenging.
 */
 
-use self::components::{AccelerationComponent, FrictionComponent, PositionComponent, VelocityComponent};
-use self::systems::{PrintStateSystem, ApplyAccelerationSystem, ApplyFrictionSystem, ApplyVelocitySystem, PrintPositionsSystem, System};
+use crate::examples::step6::dispatcher::Dispatcher;
+use crate::examples::step6::component_storage::ComponentStorage;
 
 mod components;
+mod component_storage;
+mod dispatcher;
+mod iter;
 mod systems;
 mod vector;
-mod iter;
 
 pub fn main() {
-    let mut positions = vec![
-        PositionComponent::new(0.0, 0.0),
-        PositionComponent::new(-42.0, -42.0),
-        PositionComponent::new(234.0, 123.0),
-        PositionComponent::new(6.0, 9.0),
-    ];
-    let mut velocities = vec![
-        VelocityComponent::new(40.0, 10.0),
-        VelocityComponent::new(30.0, 20.0),
-        VelocityComponent::new(20.0, 30.0),
-        VelocityComponent::new(10.0, 40.0),
-    ];
-    let frictions = vec![
-        FrictionComponent::new(1.0),
-        FrictionComponent::new(2.0),
-        FrictionComponent::new(3.0),
-        FrictionComponent::new(4.0),
-    ];
-    let accelerations = vec![
-        AccelerationComponent::new(2.0, 16.0),
-        AccelerationComponent::new(4.0, 2.0),
-        AccelerationComponent::new(8.0, 4.0),
-        AccelerationComponent::new(16.0, 8.0),
-    ];
+    // Yay! No more ugly component initialization in here! Far uglier initialization hidden inside
+    // `ComponentStorage` instead!
+    let mut components = ComponentStorage::new();
 
-    let print_state = PrintStateSystem;
-    let print_positions = PrintPositionsSystem;
-    let apply_acceleration = ApplyAccelerationSystem;
-    let apply_friction = ApplyFrictionSystem;
-    let apply_velocity = ApplyVelocitySystem;
-
-    // Note that we are no longer calling "some imported functions from global namespace", but
-    // rather `.tick()` method for instances of structs that implement `System`-trait.
-    println!("Initial state:");
-    print_state.tick((&positions, &velocities, &accelerations, &frictions));
-
-    apply_acceleration.tick((&mut velocities, &accelerations));
-    apply_friction.tick((&mut velocities, &frictions));
-    apply_velocity.tick((&mut positions, &velocities));
-
-    println!("\nPositions at the end:");
-    print_positions.tick(&positions);
+    // Yay again! We have a dumb-impl of a "dispatcher" now. (Take a look inside, it's not very
+    // sophisticated yet, but we'll get there, eventually!)
+    let dispatcher = Dispatcher::new();
+    dispatcher.dispatch(&mut components);
 }
