9be004ff7b7599915f39d2e70012a95fb46426b4
toilari <ilari97@hotmail.com>
2020-03-03 08:11:54 +0200

    Moved examples/step7 to branch part-7

--- src/component_storage.rs
+++ src/component_storage.rs
@@ -1,39 +1,230 @@
+use std::any::{Any, TypeId};
+use std::cell::{Ref, RefCell, RefMut};
+
 use crate::components::{AccelerationComponent, FrictionComponent, PositionComponent, VelocityComponent};
 
+// ...what the heck? There are two new things that are directly visible here, and lots of other new
+// stuff this allows us to do. So, let's start with the first obvious one, the `Box`.
+//
+// We are wrapping component storages to boxes here, this is called "boxing" and might be familiar
+// concept from other languages. In case it isn't, by boxing, we allocate the fields on the heap
+// instead of allocating on the stack. That means that the allocation occurs on the runtime and
+// allocated size does not need to be known at compile-time. From `std::boxed`-module documentation:
+//      "`Box` - A pointer type for heap allocation.
+//
+//       `Box<T>`, casually referred to as a 'box', provides the simplest form of heap allocation
+//       in Rust. Boxes provide ownership for this allocation, and drop their contents when they go
+//       out of scope."
+//
+// So in essence, the boxes allow us to allocate things on the heap and we only need to store 
+// a pointer (which size is obviously known at compile-time). Nice, now why is this useful?
+// Anything stored on the stack must have known size at compile time. Now that we are taking steps
+// towards unifying our component storage logic, soon we won't know beforehand (at compile-time)
+// what types of components there will be in the vectors we have in the `ComponentStorage`
+//
+// What? Why? Ok, let's try explaining that again: There is no convenient way of defining some "map"
+// data-structure that could house our component storage vectors, knowing their exact sizes from the
+// get-go. Such solution *might exist* but writing one would cause the type definitions to explode
+// out of control.
+//
+// Think about how you would store the four component boxes we have here in some data-structure.
+// What would happen to the definition of that map when we add an arbitrary number of new component
+// types? We cannot store the individual vectors in a list-of-lists as they are variable in size!
+// Writing such implementation that can store arbitrary number of vectors of arbitrary components
+// is just not feasible approach.
+//
+// So, boxes, they allow us to allocate the darn vectors on the heap, so we only need to store a
+// pointer here (again, which has known size at compile time), so presto! All our problems are gone!
+//
+//
+// Well, not *all* of our problems are gone. We just made a plenty more
+//
+// Now, if I said that the boxed `dyn Any` is a component storage vector, would you believe me?
+// If you just did, FYI, I lied, and now the code is broken because you assumed the type wrong.
+// So what is going on here actually? (Correct answer: They are `RefCell<Vec<C>>`, not `Vec<C>`)
+//
+// Compromises. Again. `Any` is a trait and `dyn` keyword just means that we are intentionally
+// defining a "trait object". Trait objects are how Rust handles polymorphism at instance level; we
+// can declare that some boxed value implements the given traits, without knowing its exact type,
+// and then use that information to dynamically dispatch the trait methods without knowing what the
+// actual implementation will be.
+//
+// Say, for example the classic `Animal` example, where we have subtypes `Dog` and `Cat` which both
+// have the `Animal`-trait. Both implement `Animal::make_noise` differently. Now, we would like to
+// store one of those in a struct called `AnimalNoisemaker`, where only thing we care is that the
+// animal making noise is an `Animal`. We can write the implementation using trait objects
+//
+//      struct AnimalNoisemaker {
+//          sound_source: Box<dyn Animal>
+//      }
+//
+// Now, we are storing a "Animal trait object" inside the `sound_source` field. Few points:
+//  1.  As the `Animal` is a trait and not a struct, we do not know its size at compile time.
+//  2.  We do not know the size? Cannot store on the stack.
+//  3.  Cannot store on the stack? Heap allocation is required.
+//  4.  Heap allocation? `Box<T>`
+//
+// Additionally:
+//  5.  Defining a trait object? Use `dyn`
+//
+// The last point has not always been the case. Older compiler versions still accept the code just
+// fine without the `dyn`-keyword. I'm not 100% sure why the keyword was added, but syntactically
+// it is now required so this is one of those "you just need to do it like this", so let's just
+// leave it at that.
+//
+//
+// Let's forget about trait objects for a moment. So, I said earlier that I lied and these are
+// actually not boxed storage vectors. What are these then?
+//
+//      positions: Box<RefCell<Vec<PositionComponent>>>,
+//
+// Above is the full type we store in the `positions`-field. The type is not known at compile-time,
+// but rather that is the *runtime* type of that field; type of the value we will store in there.
+// We do not use this full type within the struct definition to allow more flexibility for *casting
+// between the storage types* when getting a storage vector dynamically. We'll see later what this
+// actually means.
+//
+// So for now it is enough that you understand that:
+//  -   `dyn`-keyword is required when defining a trait object
+//  -   Trait object is an instance of any type which implements the required trait(s)
+//  -   Value can only be stored on the stack if it's size is known at compile time (this is called
+//      the `Sized` trait at language level)
+//  -   Trait objects (obviously as their actual type is unknown) are not `Sized` so they must be
+//      boxed.
+//  -   Boxing a value means storing it on the heap (instead of storing it on the stack). Storing
+//      on the heap means more expensive runtime memory allocation, so this should be avoided for
+//      any short-lived instances.
+//  -   Ultimatum: "These fields are boxed trait objects that implement the `Any` trait"
 pub struct ComponentStorage {
-    pub positions: Vec<PositionComponent>,
-    pub velocities: Vec<VelocityComponent>,
-    pub accelerations: Vec<AccelerationComponent>,
-    pub frictions: Vec<FrictionComponent>,
+    positions: Box<dyn Any>,
+    velocities: Box<dyn Any>,
+    accelerations: Box<dyn Any>,
+    frictions: Box<dyn Any>,
 }
 
 impl ComponentStorage {
     pub fn new() -> ComponentStorage {
+        // Now, we are allowed to do this as `RefCell` implements `Any` (Basically, `Any` is an
+        // unique trait that almost everything implements, so what we are doing here is highly
+        // illegal and wrong)
+        //
+        // These field initializers:
+        //  1.  Create a new vector containing the components
+        //  2.  Wrap the storage vector in a `RefCell` (explained later)
+        //  3.  Box the cell to move it to the heap
+        //  4.  As type of `positions` is `Box<dyn Any>`, from compiler perspective, all we know 
+        //      about it from now on is that it implements `Any`. Yes, we just lost type-safety.
         ComponentStorage {
-            positions: vec![
+            positions: Box::new(RefCell::new(vec![
                 PositionComponent::new(0.0, 0.0),
                 PositionComponent::new(-42.0, -42.0),
                 PositionComponent::new(234.0, 123.0),
                 PositionComponent::new(6.0, 9.0),
-            ],
-            velocities: vec![
+            ])),
+            velocities: Box::new(RefCell::new(vec![
                 VelocityComponent::new(40.0, 10.0),
                 VelocityComponent::new(30.0, 20.0),
                 VelocityComponent::new(20.0, 30.0),
                 VelocityComponent::new(10.0, 40.0),
-            ],
-            frictions: vec![
+            ])),
+            frictions: Box::new(RefCell::new(vec![
                 FrictionComponent::new(1.0),
                 FrictionComponent::new(2.0),
                 FrictionComponent::new(3.0),
                 FrictionComponent::new(4.0),
-            ],
-            accelerations: vec![
+            ])),
+            accelerations: Box::new(RefCell::new(vec![
                 AccelerationComponent::new(2.0, 16.0),
                 AccelerationComponent::new(4.0, 2.0),
                 AccelerationComponent::new(8.0, 4.0),
                 AccelerationComponent::new(16.0, 8.0),
-            ],
+            ])),
         }
+    }
+
+    // Next up, the actual, anti-climatic "magic-trick" to allow fetching component storages by
+    // only knowing its component type. There are two variants, `fetch_mut` and `fetch_ref` for
+    // fetching storages as mutable or immutable.
+    
+    // Fetch a mutable reference to a component storage with component type `C`
+    pub fn fetch_mut<C>(&self) -> RefMut<Vec<C>>
+        where C: Any // `C` must implement `Any` so that we can use it in `TypeId::of::<C>()`
+    {
+        // This is the first trick. Basically, this counts as runtime reflection; we are fetching
+        // type information dynamically at runtime. Although, this is nothing more special than
+        // fetching a compiler assigned type-id of the component type. `TypeId::of` is guaranteed
+        // to have the same return value for two equal invocations (if `C` is the same, that is)
+        let component_type_id = TypeId::of::<C>();
+
+        // Now, we have a `u64` wrapped into a `TypeId`, describing which component type we are
+        // working with. Get box the storage cell is in
+        let storage_raw = {
+            // Now, as we are writing implementation for fixed number of components, just treat each 
+            // component type as a special-case and panic if type is anything else.
+            if component_type_id == TypeId::of::<PositionComponent>() {
+                // We do not wish to move the positions out of `self`, so return as a reference
+                &self.positions
+            } else if component_type_id == TypeId::of::<VelocityComponent>() {
+                &self.velocities
+            } else if component_type_id == TypeId::of::<AccelerationComponent>() {
+                &self.accelerations
+            } else if component_type_id == TypeId::of::<FrictionComponent>() {
+                &self.frictions
+            } else {
+                panic!("Unknown component type!")
+            } // No semicolon, selected component cell is treated as return value
+        };
+
+        // Now, this is where it gets interesting. We have a `Box<dyn Any>`, but with the if-else
+        // spaghetti above, we have verified that for this specific box, the equivalence
+        //
+        //      Box<dyn Any> == Box<RefCell<Vec<C>>>
+        //
+        // holds. The sad thing is that the compiler is not as smart (or stupid) as we are. So, we
+        // somehow need to tell the compiler that these are actually the same type. This is called
+        // "downcasting a pointer" (box is essentially a pointer). 
+        //
+        // Luckily, `dyn Any` implements a `downcast_ref/mut` for downcasting the pointer mutably or
+        // immutably to any type. The cast then returns an `Option` based on whether or not the cast
+        // was actually possible.
+        //
+        // So:
+        //  1.  We have a pointer to `dyn Any`
+        //  2.  We know that this pointer points to data that has type `RefCell<Vec<C>>`
+        //  3.  Trait object `dyn Any` has a method `downcast_ref`, which we can use to downcast the
+        //      pointer to any other type, bypassing compiler restrictions
+        //  4.  Downcast returns an `Option`, we expect it to be `Some(value)` and panic with error
+        //      message if it is `None`
+        //  5.  We now have the `RefCell<Vec<C>>`
+        let storage = storage_raw.downcast_ref::<RefCell<Vec<C>>>()
+            .expect("Downcasting mutable storage RefCell failed!");
+        
+        // Borrow the value inside the `RefCell` mutably and return the reference wrapper
+        storage.borrow_mut()
+    }
+
+    // The very same thing as the above, but we borrow immutably instead of mutably at the very end
+    pub fn fetch_ref<C>(&self) -> Ref<Vec<C>>
+        where C: Any
+    {
+        let component_type_id = TypeId::of::<C>();
+
+        let storage_raw = {
+            if component_type_id == TypeId::of::<PositionComponent>() {
+                &self.positions
+            } else if component_type_id == TypeId::of::<VelocityComponent>() {
+                &self.velocities
+            } else if component_type_id == TypeId::of::<AccelerationComponent>() {
+                &self.accelerations
+            } else if component_type_id == TypeId::of::<FrictionComponent>() {
+                &self.frictions
+            } else {
+                panic!("Unknown component type!")
+            }
+        };
+
+        let storage = storage_raw.downcast_ref::<RefCell<Vec<C>>>()
+            .expect("Downcasting immutable storage RefCell failed!");
+        storage.borrow() // This is the only difference to when compared the `fetch_mut`
     }
 }
--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -1,5 +1,6 @@
-use crate::systems::{ApplyAccelerationSystem, ApplyFrictionSystem, ApplyVelocitySystem, PrintPositionsSystem, PrintStateSystem, System};
 use crate::component_storage::ComponentStorage;
+use crate::components::{AccelerationComponent, FrictionComponent, PositionComponent, VelocityComponent};
+use crate::systems::{ApplyAccelerationSystem, ApplyFrictionSystem, ApplyVelocitySystem, PrintPositionsSystem, PrintStateSystem, System};
 
 pub struct Dispatcher {
     print_state: PrintStateSystem,
@@ -20,15 +21,87 @@ impl Dispatcher {
         }
     }
 
-    pub fn dispatch(&self, components: &mut ComponentStorage) {
+    // Fun fact:
+    //  The component storage does not need to be mutable even though we technically mutate its
+    //  contents. This is again due to `RefCell` moving the ownership of the data elsewhere, so
+    //  mutating the data inside the cell isn't actually mutating the storage (as it does not own
+    //  the data, it just holds a pointer to it). We are declaring the storage as mutable anyway,
+    //  because it would be quite confusing that the contents of the storage mutate while
+    //  the storage is being borrowed immutably.
+    pub fn dispatch(&self, storage: &mut ComponentStorage) {
+        // This is the actual reason why `RefCells` are needed (If you haven't take a look at
+        // `component_storage.rs` now).
+        //
+        // So, instead of referring directly to the fields on the storage, we are using `fetch_ref`
+        // and `fetch_mut` to get references to the component vectors. As returned values are of
+        // types `Ref<T>` and `RefMut<T>`, we use `as_ref` and `as_mut` to convert them into raw
+        // references (systems still want them as `&Vec<C>` not `Ref<Vec<C>>` and `as_ref`
+        // performs the conversion)
+        //
+        // However, there can only be one simultaneous mutable borrow of an object at once (multiple
+        // simultaneous immutable borrows are legal and called "aliasing"). Additionally, we cannot
+        // borrow as immutable if we have already borrowed as mutable. This means that only the
+        // `print_state` and `print_positions` tick calls would be legal if storage was allowed to
+        // be borrowed mutably (those two systems make only immutable borrows, they are read-only).
         println!("State before tick:");
-        self.print_state.tick((&components.positions, &components.velocities, &components.accelerations, &components.frictions));
+        self.print_state.tick((
+            storage.fetch_ref::<PositionComponent>().as_ref(),
+            storage.fetch_ref::<VelocityComponent>().as_ref(),
+            storage.fetch_ref::<AccelerationComponent>().as_ref(),
+            storage.fetch_ref::<FrictionComponent>().as_ref(),
+        ));
 
-        self.apply_acceleration.tick((&mut components.velocities, &components.accelerations));
-        self.apply_friction.tick((&mut components.velocities, &components.frictions));
-        self.apply_velocity.tick((&mut components.positions, &components.velocities));
+        // This is example of a situation where we have more information about execution of the
+        // program than the compiler. Why does the compiler prevent this if `storage` was borrowed
+        // mutably?
+        //  1.  In this situation, `storage.fetch_mut()` would have `&mut self` parameter, meaning
+        //      that we borrowed it mutably when evaluating the first parameter
+        //  2.  Borrow does not end when the method returns, as the returned value refers to
+        //      some storage vector, obtained from the borrowed storage (cannot release the borrow
+        //      before all references to borrowed value are released)
+        //  3.  Now, the second parameter `storage.fetch_ref()` has `&self`, which means that we are
+        //      trying to perform an immutable borrow, but the first parameter still holds mutable
+        //      borrow! We cannot borrow immutably while there a mutable one still exists!
+        //
+        // But this is wrong! We clearly see that the first parameter ends up getting storage vector
+        // for `VelocityComponent` and second parameter gets `AccelerationComponent`. There is no
+        // risk of simultaneous mutable access, but the compiler does not know that; all it sees is
+        // that there is simultaneous mutable and immutable access into `storage` itself, and that's
+        // illegal.
+        //
+        // So, what have we done to circumvent this? The `RefCell` sort-of allows us to *bypass the
+        // compile-time borrow checks* and perform the borrow-checking on runtime instead. Yes, this
+        // again is a powerful ability, best used sparingly as there is a whole lot that can go
+        // wrong.
+        //
+        // In this case, this allows us to always borrow the storage immutably. Why? We are not
+        // mutating the storage anymore. All we are doing with the storage, is getting the stored
+        // *pointer* inside the `RefCell` and then borrowing the memory that cell points to. This
+        // borrow happens purely during the runtime and does not require storage to be mutable, thus
+        // we are allowed to borrow individual storage vectors as we wish.
+        //
+        // However, if one system holds any storage vector as mutable, and another system tries to
+        // borrow that before the first system has a chance to release the borrow, the program will
+        // crash with a panic. That is, the borrow rules are still in place, they just are not
+        // checked at compile time.
+        self.apply_acceleration.tick((
+            storage.fetch_mut::<VelocityComponent>().as_mut(),
+            storage.fetch_ref::<AccelerationComponent>().as_ref(),
+        ));
+
+        self.apply_friction.tick((
+            storage.fetch_mut::<VelocityComponent>().as_mut(),
+            storage.fetch_ref::<FrictionComponent>().as_ref(),
+        ));
+
+        self.apply_velocity.tick((
+            storage.fetch_mut::<PositionComponent>().as_mut(),
+            storage.fetch_ref::<VelocityComponent>().as_ref(),
+        ));
 
         println!("\nPositions after tick:");
-        self.print_positions.tick(&components.positions);
+        self.print_positions.tick(
+            storage.fetch_ref::<PositionComponent>().as_ref()
+        );
     }
 }
