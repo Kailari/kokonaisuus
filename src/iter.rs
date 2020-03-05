5f93fe01f6310647070523b148397b5f919062b8
toilari <ilari97@hotmail.com>
2020-03-05 13:25:39 +0200

    Moved part 4 documentation to the book

--- src/iter.rs
+++ src/iter.rs
@@ -7,59 +7,13 @@ pub trait IteratorTuple {
     fn next_all(&mut self) -> Option<Self::ItemTuple>;
 }
 
-// The `IteratorTuple` implementation moved from individual systems here. There is a fair bit of
-// things to chew on.
-//
-// Ok, let's go; we had a lot of duplicate code in our `IteratorTuple` implementations and we would
-// like to extract the common behavior to a single implementation. That is:
-//      "I would like to implement `IteratorTuple` on all permutations of `IterMut` or `Iter` on any
-//       permutation of components"
-//
-// At first sight, this is seemingly impossible. The number of permutations quickly explodes out of
-// control. Luckily, after pondering on this a bit, we notice that for all of our systems, two
-// conditions are fulfilled:
-//      1.  the iterator tuples are always 2-tuples
-//      2.  all of the iterators in those tuples have a common level of abstraction, namely,
-//          the `Iterator`-trait
-//
-// NOTE: (printer system is not counted in as it can use its single iterator without any trickery)
-//
-// So, those two observations in mind, our seemingly impossible issue boils down to something that
-// suddenly seems much more manageable:
-//      "I would like to implement `IteratorTuple` on all 2-tuples of iterators"
-//
-// Now, let's get technical for a bit. The `impl`-block is allowed to define arbitrary number of
-// type parameters, as long as we constraint them by using them in the trait or the target type.
-// In other words:
-//
-//      impl<A, B> SomeTrait<A> for SomeStruct<B>   // ok!
-//      impl<A, B> SomeTrait<A, B> for SomeStruct   // ok!
-//      impl<A, B> SomeTrait for SomeStruct<A, B>   // ok!
-//      impl<A, B> SomeTrait<(A, B)> for SomeStruct // ok!
-//      impl<A, B> SomeTrait<A> for SomeStruct      // error! type parameter B is unconstrained!
-//      impl<A, B> SomeTrait for SomeStruct<B>      // error! type parameter A is unconstrained!
-//      // etc.
-//
-// Now how would that be useful to us?
-//      "I would like to implement `IteratorTuple`..."  =>  impl<...> IteratorTuple for ...
-//      "...on all 2-tuples..."                         =>  impl<A, B> IteratorTuple for (A, B)
-//      "...of iterators"                               =>      where A: Iterator,
-//                                                                    B: Iterator
-//
-// See, when you can't find the answer, make sure you are asking the right questions!
-//
 impl<A, B> IteratorTuple for (A, B)
     where A: Iterator,
           B: Iterator
 {
-    // Here we don't need to use the fully qualified syntax (`<A as Iterator>::Item`), as we already
-    // know from the trait bounds that `A` and `B` are iterators.
     type ItemTuple = (A::Item, B::Item);
 
     fn next_all(&mut self) -> Option<Self::ItemTuple> {
-        // `self` is a 2-tuple (A, B), where A and B are iterators, so it's quite natural we are
-        // allowed to call `.next()` on them. We've seen this same matcher pattern before so many
-        // times I won't go to detail on how it works.
         match (self.0.next(), self.1.next()) {
             (Some(pos), Some(vel)) => Some((pos, vel)),
             _ => None,
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,5 @@
 #![feature(assoc_int_consts)] // Allows use of things like `f64::EPSILON`
 
-/*
-Step 4. Generifying IteratorTuples
-Topics: Arbitrary type parameters in trait implementation
-
-New here:
-    -   Single implementation for all 2-tuples of iterators. This can be found in `iter.rs`
-    -   Other than that, only changes to other files are the fact that the boilerplate-
-        implementations of `IteratorTuple` have magically disappeared from system modules.
-
-Notes:
-    So, last time we added concept of `IteratorTuple`s using a custom trait. This required us to
-    write a lot of boilerplate in system modules. This time, we want to reduce that boilerplate down
-    to a single implementation using a few tricks involving generics.
-
-    Note also that our implementation does not care whether or not the specific iterators are
-    `IterMut`, `Iter` or some other iterator completely. We can use a single implementation for all
-    those cases using the lowest common level of abstraction, the `Iterator`-trait.
-
-    Now, we are limited to 2-tuples as that's the only tuple we've implemented `IteratorTuple` on.
-    In next part, we'll look into using macros to generate us a metric ton of other similar
-    implementations on larger tuples.
-*/
-
 use self::components::{AccelerationComponent, FrictionComponent, PositionComponent, VelocityComponent};
 use self::systems::{apply_velocity, print_positions, apply_friction, apply_acceleration};
 
