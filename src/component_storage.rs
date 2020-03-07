0c2422ffd1048c153c079b543c5704abda8e175f
toilari <ilari97@hotmail.com>
2020-03-07 16:34:59 +0200

    Component types are now registered dynamically

--- src/component_storage.rs
+++ src/component_storage.rs
@@ -1,43 +1,20 @@
 use std::any::{Any, TypeId};
 use std::cell::{Ref, RefCell, RefMut};
-
-use crate::components::{AccelerationComponent, FrictionComponent, PositionComponent, VelocityComponent};
+use std::collections::HashMap;
 
 pub struct ComponentStorage {
-    positions: RefCell<Vec<PositionComponent>>,
-    velocities: RefCell<Vec<VelocityComponent>>,
-    accelerations: RefCell<Vec<AccelerationComponent>>,
-    frictions: RefCell<Vec<FrictionComponent>>,
+    components: HashMap<TypeId, Box<dyn Any>>,
 }
 
 impl ComponentStorage {
     pub fn new() -> ComponentStorage {
-        ComponentStorage {
-            positions: RefCell::new(vec![
-                PositionComponent::new(0.0, 0.0),
-                PositionComponent::new(-42.0, -42.0),
-                PositionComponent::new(234.0, 123.0),
-                PositionComponent::new(6.0, 9.0),
-            ]),
-            velocities: RefCell::new(vec![
-                VelocityComponent::new(40.0, 10.0),
-                VelocityComponent::new(30.0, 20.0),
-                VelocityComponent::new(20.0, 30.0),
-                VelocityComponent::new(10.0, 40.0),
-            ]),
-            frictions: RefCell::new(vec![
-                FrictionComponent::new(1.0),
-                FrictionComponent::new(2.0),
-                FrictionComponent::new(3.0),
-                FrictionComponent::new(4.0),
-            ]),
-            accelerations: RefCell::new(vec![
-                AccelerationComponent::new(2.0, 16.0),
-                AccelerationComponent::new(4.0, 2.0),
-                AccelerationComponent::new(8.0, 4.0),
-                AccelerationComponent::new(16.0, 8.0),
-            ]),
-        }
+        ComponentStorage { components: HashMap::new() }
+    }
+
+    pub fn register_component_type<C>(&mut self, components: Vec<C>)
+        where C: Any
+    {
+        self.components.insert(TypeId::of::<C>(), Box::new(RefCell::new(components)));
     }
 
     pub fn fetch_mut<C>(&self) -> RefMut<Vec<C>>
@@ -58,20 +35,10 @@ impl ComponentStorage {
         where C: Any
     {
         let component_type_id = TypeId::of::<C>();
-        let storage =
-            if component_type_id == TypeId::of::<PositionComponent>() {
-                &self.positions as &dyn Any
-            } else if component_type_id == TypeId::of::<VelocityComponent>() {
-                &self.velocities as &dyn Any
-            } else if component_type_id == TypeId::of::<AccelerationComponent>() {
-                &self.accelerations as &dyn Any
-            } else if component_type_id == TypeId::of::<FrictionComponent>() {
-                &self.frictions as &dyn Any
-            } else {
-                panic!("Unknown component type!")
-            };
+        let storage = self.components.get(&component_type_id)
+                          .unwrap_or_else(|| panic!("Use of an unregistered component type: {}",
+                                                    std::any::type_name::<C>()));
 
-        storage.downcast_ref::<RefCell<Vec<C>>>()
-               .expect("Downcasting storage RefCell failed!")
+        storage.downcast_ref::<RefCell<Vec<C>>>().unwrap()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-#![feature(assoc_int_consts)] // Allows use of things like `f64::EPSILON`
-
-use crate::dispatcher::Dispatcher;
 use crate::component_storage::ComponentStorage;
+use crate::components::{AccelerationComponent, FrictionComponent, PositionComponent, VelocityComponent};
+use crate::dispatcher::Dispatcher;
 
 mod components;
 mod component_storage;
@@ -11,7 +10,37 @@ mod systems;
 mod vector;
 
 pub fn main() {
-    let mut components = ComponentStorage::new();
+    let mut storage = ComponentStorage::new();
+    let positions = vec![
+        PositionComponent::new(0.0, 0.0),
+        PositionComponent::new(-42.0, -42.0),
+        PositionComponent::new(234.0, 123.0),
+        PositionComponent::new(6.0, 9.0),
+    ];
+    let velocities = vec![
+        VelocityComponent::new(40.0, 10.0),
+        VelocityComponent::new(30.0, 20.0),
+        VelocityComponent::new(20.0, 30.0),
+        VelocityComponent::new(10.0, 40.0),
+    ];
+    let frictions = vec![
+        FrictionComponent::new(1.0),
+        FrictionComponent::new(2.0),
+        FrictionComponent::new(3.0),
+        FrictionComponent::new(4.0),
+    ];
+    let accelerations = vec![
+        AccelerationComponent::new(2.0, 16.0),
+        AccelerationComponent::new(4.0, 2.0),
+        AccelerationComponent::new(8.0, 4.0),
+        AccelerationComponent::new(16.0, 8.0),
+    ];
+
+    storage.register_component_type(positions);
+    storage.register_component_type(velocities);
+    storage.register_component_type(accelerations);
+    storage.register_component_type(frictions);
+
     let dispatcher = Dispatcher::new();
-    dispatcher.dispatch(&mut components);
+    dispatcher.dispatch(&mut storage);
 }
